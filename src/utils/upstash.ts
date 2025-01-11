2b0fc03150080ffe2c5fee793fc7675b37d58d82
Anshuman Bhardwaj <anshu5074@gmail.com>
2025-01-11 17:26:19 +0530

    add cache to interests

--- src/utils/upstash.ts
+++ src/utils/upstash.ts
@@ -1,6 +1,7 @@
 import { Redis } from "@upstash/redis";
 import { Interest } from "./types";
 import { BANKS } from "./constants";
+import { unstable_cache } from "next/cache";
 
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
@@ -16,14 +17,18 @@ export async function getInterestRates(key: string): Promise<Interest> {
   return data as Interest;
 }
 
-export async function getInterestRatesAll() {
-  const results = await Promise.all(
-    BANKS.map(async (bank) => ({
-      key: bank.key,
-      name: bank.name,
-      url: bank.url,
-      interest: await getInterestRates(`interest-${bank.key}`),
-    }))
-  );
-  return results;
-}
+export const getInterestRatesAll = unstable_cache(
+  async function () {
+    const results = await Promise.all(
+      BANKS.map(async (bank) => ({
+        key: bank.key,
+        name: bank.name,
+        url: bank.url,
+        interest: await getInterestRates(`interest-${bank.key}`),
+      }))
+    );
+    return results;
+  },
+  ["all-interests"],
+  { revalidate: 3600, tags: ["all-interests"] }
+);
