5568226dc070150167c8ff277d9f7d84c41611ea
Anshuman Bhardwaj <anshu5074@gmail.com>
2025-01-11 17:28:38 +0530

    add suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import BankInterestRates from "@/components/InterestTable";
 import { getInterestRatesAll } from "@/utils/upstash";
+import { Suspense } from "react";
 
 export default async function HomePage() {
   const data = await getInterestRatesAll();
@@ -7,7 +8,9 @@ export default async function HomePage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Bank Interest Rates</h1>
-      <BankInterestRates bankData={data} />
+      <Suspense>
+        <BankInterestRates bankData={data} />
+      </Suspense>
     </div>
   );
 }
